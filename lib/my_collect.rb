eb0f568c83e13deb416b76398502ee6e3b5c03be
Victoria Curl <victoriacurl9@gmail.com>
2019-12-22 15:53:36 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -2,8 +2,8 @@ def my_collect(collects)
   i = 0
 new_collection = []
   while i < collects.length
-   binding.pry
-   yield(array[i])
+    new_collection << yield(collects[i])
    i += 1
  end
+ new_collection
  end
